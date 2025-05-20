a7284c25fcfb7900f7beeb05c1bea01c55a3c4f6
luc-10 <isdalyt@gmail.com>
2025-05-20 23:00:08 +0200

    Day 10

---
+++ day10/day10b.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using std::cout;
+using std::ifstream;
+using std::vector;
+
+int countTrails(vector<vector<int>> &mat, int i, int j, int num)
+{
+    if (i < 0 || j < 0 || i >= mat.size() || j >= mat[i].size() || mat[i][j] != num)
+        return 0;
+    else if (mat[i][j] == 9)
+    {
+        cout << "Finished at " << i << ' ' << j << '\n';
+        return 1;
+    }
+    else
+    {
+        cout << "Now at " << i << ' ' << j << '\n';
+        int ret = 0;
+        ret += countTrails(mat, i + 1, j, num + 1);
+        ret += countTrails(mat, i - 1, j, num + 1);
+        ret += countTrails(mat, i, j + 1, num + 1);
+        ret += countTrails(mat, i, j - 1, num + 1);
+        return ret;
+    }
+}
+
+int main()
+{
+    ifstream file("input.txt");
+    char c;
+    int index = 0;
+    vector<vector<int>> mat(0, vector<int>(0));
+    while (file.get(c))
+    {
+        if (c == '\n')
+        {
+            index++;
+            continue;
+        }
+        if (index >= mat.size())
+            mat.resize(index + 1);
+        mat[index].push_back(c - '0');
+    }
+    int trails = 0;
+    for (int i = 0; i < mat.size(); i++)
+    {
+        for (int j = 0; j < mat[i].size(); j++)
+        {
+            if (mat[i][j] == 0)
+            {
+                cout << "Starting trail at " << i << ' ' << j << '\n';
+                trails += countTrails(mat, i, j, 0);
+            }
+        }
+    }
+    cout << trails << '\n';
+    return 0;
+}
